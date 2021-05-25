5a6918edd8a3924b7be7fe2b9f8da3e4c5612c4c
Pablo Gómez Ponce <pgomezponce@gmail.com>
2021-05-25 23:19:45 +0200

    Demo Account

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Link, Text } from "./texts/content";
+import {Heading, Title} from "./texts/titles";
+import AssetProvider from "./utils/AssetProvider";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AssetProvider>
+      <View style={styles.container}>
+        <Title>
+          Hello <Text sup>world</Text> !
+        </Title>
+        <Heading>Demo header</Heading>
+        <Text italic style={{ fontFamily: "bold" }}>
+          Open up App.js to start working on your app!
+          <Text sup bold>
+            Demo
+          </Text>
+          <Link href="https://www.google.es">Hello</Link>
+        </Text>
+
+        <StatusBar style="auto" />
+      </View>
+    </AssetProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
